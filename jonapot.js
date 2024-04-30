ef6682254100a9df040b3c941197f8d7966118c7
Csongoranevem <98952021+Csongoranevem@users.noreply.github.com>
2024-04-30 14:12:45 +0200

    Források feltöltése

---
+++ jonapot.js
@@ -0,0 +1,14 @@
+function KoKiValaszt() {
+    let szegely = document.getElementById("ko")
+    szegely.style.border.match("0px") ? szegely.style.border = "3px solid red" : szegely.style.border = "0px"
+}
+
+function PapKiValaszt() {
+    
+    szegely.style.border.match("0px") ? szegely.style.border = "3px solid red" : szegely.style.border = "0px"
+}
+
+function OlloKiValaszt() {
+    let szegely = document.getElementById("ollo")
+    szegely.style.border.match("0px") ? szegely.style.border = "3px solid red" : szegely.style.border = "0px"
+}
